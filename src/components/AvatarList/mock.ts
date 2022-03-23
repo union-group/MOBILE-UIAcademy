a779b28a97c815eba9bfbdcb0d26ff6c9e2535f1
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2022-03-23 20:05:27 -0300

    feat: add AvatarList

---
+++ src/components/AvatarList/mock.ts
@@ -0,0 +1,31 @@
+import { AvatarListProps } from '.'
+
+export const mockAvatar: AvatarListProps = {
+  imageList: [
+    {
+      id: '1',
+      imgUrl:
+        'https://res.cloudinary.com/dbnq26wqe/image/upload/v1648093889/others/image_04_nrxubl.jpg',
+    },
+    {
+      id: '2',
+      imgUrl:
+        'https://res.cloudinary.com/dbnq26wqe/image/upload/v1648093889/others/image_03_e3vugv.jpg',
+    },
+    {
+      id: '3',
+      imgUrl:
+        'https://res.cloudinary.com/dbnq26wqe/image/upload/v1648093889/others/image_05_ww4pum.jpg',
+    },
+    {
+      id: '4',
+      imgUrl:
+        'https://res.cloudinary.com/dbnq26wqe/image/upload/v1648093889/others/image_05_ww4pum.jpg',
+    },
+    {
+      id: '5',
+      imgUrl:
+        'https://res.cloudinary.com/dbnq26wqe/image/upload/v1648093889/others/image_05_ww4pum.jpg',
+    },
+  ],
+}
