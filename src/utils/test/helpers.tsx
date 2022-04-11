5659b2a7e88892ce92a5e1c38f67fe78ab7a5cea
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-04-11 14:03:22 -0300

    chore: add renderWithHook function on utils

--- src/utils/test/helpers.tsx
+++ src/utils/test/helpers.tsx
@@ -1,8 +1,36 @@
 import React, { ReactElement } from 'react'
 import { ThemeProvider } from 'styled-components/native'
-import { render } from '@testing-library/react-native'
+import { render, RenderOptions } from '@testing-library/react-native'
 
 import { theme } from '../../styles/theme'
 
+import {
+  VideoContext,
+  VideoContextData,
+  VideoContextDefaultValues,
+} from '../../hooks/useVideo'
+
+type CustomRenderOptions = Omit<RenderOptions, 'queries'>
+
+interface CustomRenderProps extends CustomRenderOptions {
+  videoProviderProps?: VideoContextData
+}
+
 export const renderWithTheme = (component: ReactElement) =>
   render(<ThemeProvider theme={theme}>{component}</ThemeProvider>)
+
+export const renderWithHook = (
+  ui: ReactElement,
+  {
+    videoProviderProps = VideoContextDefaultValues,
+    ...renderOptions
+  }: CustomRenderProps = {},
+) =>
+  render(
+    <ThemeProvider theme={theme}>
+      <VideoContext.Provider value={videoProviderProps}>
+        {ui}
+      </VideoContext.Provider>
+    </ThemeProvider>,
+    renderOptions,
+  )
