e5d07daf067a8937794f7e0d69a8b10aaa0ec5de
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2022-05-12 17:57:24 -0300

    feat: create screen Favorite

---
+++ src/screens/Favorite/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as S from './styles'
+
+export const Favorite = () => {
+  return (
+    <S.Container>
+      <S.Text>Favorite page</S.Text>
+    </S.Container>
+  )
+}
