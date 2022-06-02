fa01847155ecb0a3882910e771eeba67fb91d4fe
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2022-06-02 11:05:44 -0300

    style: add new colors

--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -1,13 +1,18 @@
 export const colors = {
   primary: '#C2E7D6',
+  primaryBlue: '#0048FE',
+  primaryPurple: '#851D86',
   primaryDark: '#82D9B1',
 
-  secondary: '#F6F6FA',
+  secondary: '#19191B',
 
   white: '#FFFFFF',
   black: '#0B0B0B',
 
   neutral: '#E1E1E1',
+  neutral200: '#C1C1C1',
+  neutral500: '#7A7A7A',
+  neutral700: '#252525',
 
-  background: '#F6F6FA',
+  background: '#19191B',
 } as const
